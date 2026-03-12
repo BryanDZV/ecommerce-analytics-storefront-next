5b69fce46a9d09f99aeb2aa853a4beaa8a14697f
Luz Bietti <luz.bietti@globant.com>
2026-03-12 15:56:36 +0100

    styles: add styles of header in global

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,22 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-[url(/header.jpg)] h-40 bg-cover">
-      <div className="pt-5">
-        <hr className="w-60" />
-      </div>
-      <div className="ml-10 w-20">
-        <h2 className="">Next Commerce</h2>
-      </div>
-      <div className="w-80">
-        <hr />
-        <p>
-          Add here any awards or things you want to highlight. Could also be a
-          brief summary about the project you worked on.
-        </p>
+    <header className="header">
+      <div className="main-container-header">
+        <div className="container-logo">
+          {/* <hr className="first-line" /> */}
+          <div className="logo-image">
+            <img src="/Nextcom.png" alt="" />
+          </div>
+        </div>
+
+        <div className="description-box">
+          <hr />
+          <p>
+            Add here any awards or things you want to highlight. Could also be a
+            brief summary about the project you worked on.
+          </p>
+        </div>
       </div>
     </header>
   );
