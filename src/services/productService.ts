f6abe0d8d5539560e3761abef33cbf6234085124
Bryan-Zavala <bryan.zavala@globant.com>
2026-03-16 17:21:52 +0100

    feat(services):allow filtering products by multiple categories

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,20 +1,21 @@
+import { Product } from '@/types/product';
 import { products } from '@/mocks/mockProducts';
 
 //function to get all products from the simulated database
-export const getProducts = () => {
+export const getProducts = (): Product[] => {
   return products;
 };
 
 //function to get a product and view its details
-export const getProductById = (id: string) => {
+export const getProductById = (id: string): Product | undefined => {
   return products.find((p) => {
     return p.id === id;
   });
 };
+//function that gives an array with the products that belong to the categories selected by the user.
 
-//function that gives an array with the products of a specific category
-export const getProductsByCategory = (category: string) => {
-  return products.filter((p) => {
-    return p.category === category;
-  });
+export const getProductsByCategory = (categories: string[]): Product[] => {
+  return !categories || categories.length === 0
+    ? getProducts()
+    : products.filter((p) => categories.includes(p.category));
 };
