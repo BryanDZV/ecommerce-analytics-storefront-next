3958c43443b8e14c2493cb850b40df6e3a0e83df
Bryan-Zavala <bryan.zavala@globant.com>
2026-03-17 11:23:13 +0100

    refactor(hooks): group product hooks and implement hierarchical query keys

--- src/hooks/products/useProducts.ts
+++ src/hooks/products/useProducts.ts
@@ -1,11 +1,36 @@
-import { getProducts } from '@/services/productService';
-import { PaginatedResponse } from '@/types/paginatedResponse';
-import { Product } from '@/types/product';
 import { useQuery } from '@tanstack/react-query';
+import {
+  getProducts,
+  getProductById,
+  getProductsByCategory,
+} from '@/services/productService';
+import { Product } from '@/types/product';
+import { PaginatedResponse } from '@/types/paginatedResponse';
 
-export const useProducts = () => {
+//  Hook for general list
+export const useListProducts = () => {
   return useQuery<PaginatedResponse<Product>>({
-    queryKey: ['products'],
+    // Structure: [Domain, Action]
+    queryKey: ['products', 'list'],
     queryFn: () => getProducts(),
+  });
+};
+
+//  Hook for detail (DYNAMIC)
+export const useProduct = (id: string) => {
+  return useQuery<Product | undefined>({
+    // Structure: [Domain, Action, ID]
+    queryKey: ['products', 'detail', id],
+    queryFn: () => getProductById(id),
+  });
+};
+
+//  Hook for categories (DYNAMIC with Object)
+export const useProductsByCategories = (categories: string[]) => {
+  return useQuery<Product[]>({
+    // Structure: [Domain, Action, { Filters }]
+    // the key will be ['products', 'list', { categories: ['electronics'] }]
+    queryKey: ['products', 'list', { categories }],
+    queryFn: () => getProductsByCategory(categories),
   });
 };
