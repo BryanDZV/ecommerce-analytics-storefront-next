105cf0835a421c4588f89e15d03d3e0502f1696c
Luis Rodríguez Camas <luis.rodriguez02@globant.com>
2026-03-18 10:30:35 +0100

    feat: add dynamic import for login modal component

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,12 +1,18 @@
-'use client'
+'use client';
 
-import Image from 'next/image';
 import Filtericon from './buttons/FilterIcon';
-import LoginModal from './LoginModal'
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const LoginModal = dynamic(() => import('../components/LoginModal'), {
+  loading: () => <p>Cargando...</p>,
+
+  // Disables Server-side rendering
+  ssr: false,
+});
 
 export default function Searchbar() {
-  const [isLoginOpen, setIsLoginOpen] = useState(false)
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
   return (
     <>
       <section>
@@ -26,8 +32,12 @@ export default function Searchbar() {
             </div>
             <div className="cart-user">
               <img className="filter-image" src="/shopping-cart.png" alt="" />
-              <button type='button' onClick={() => setIsLoginOpen(true)}>
-              <img className="filter-image" src="/person.png" alt="user-icon" />
+              <button type="button" onClick={() => setIsLoginOpen(true)}>
+                <img
+                  className="filter-image"
+                  src="/person.png"
+                  alt="user-icon"
+                />
               </button>
             </div>
           </div>
@@ -38,10 +48,13 @@ export default function Searchbar() {
           </div>
         </div>
       </section>
-      <LoginModal
-        isOpen={isLoginOpen}
-        onClose={() => setIsLoginOpen(false)}
-      />  
+
+      {isLoginOpen ? (
+        <LoginModal
+          isOpen={isLoginOpen}
+          onClose={() => setIsLoginOpen(false)}
+        />
+      ) : null}
     </>
   );
 }
