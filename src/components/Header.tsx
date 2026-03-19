62d198bb5d12003fb883421d4ad1bfe010788edf
Luis Rodríguez Camas <luis.rodriguez02@globant.com>
2026-03-19 13:24:39 +0100

    feat: improve lcp
    Added FlexibleImage component as a background Image instead of using tailwind styles to improve performance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,20 @@
-import Image from 'next/image';
+import FlexibleImage from '@/app/components/FlexibleImage';
 
 export default function Header() {
   return (
     <header className="header">
+      {/* --- BACKGROUND IMAGE --- */}
+      <div className="absolute inset-0 z-0">
+        <FlexibleImage
+          src="/header.jpg"
+          alt="Fondo de cabecera"
+          fill
+          priority
+          sizes='100vw'
+          className='object-cover'
+        />
+      </div>
+      
       <div className="main-container-header">
         <div className="container-logo">
           {/* <hr className="first-line" /> */}
